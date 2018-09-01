f53f3b9d9fe02dd9c6ea8531f8efc657c5062a43
ei <chronolavos0124@gmail.com>
2018-09-01 16:21:53 +0900

    Add option

--- head.c
+++ head.c
@@ -1,12 +1,58 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define _GNU_SOURCE
+#include <getopt.h>
+
 static void do_head(FILE *f, long nlines);
+
+#define DEFAULT_N_LINES 10
+
+static struct option longopts[] = {
+    {"lines", required_argument, NULL, 'n'},
+    {"help", no_argument,        NULL, 'h'},
+    {0, 0, 0, 0}
+};
 
 int main(int argc, char *argv[])
 {
-    long nlines;
+    int opt;
+    long nlines = DEFAULT_N_LINES;
 
+    while ((opt = getopt_long(argc, argv, "n:", longopts, NULL)) != -1) {
+        switch (opt) {
+            case 'n':
+                nlines = atoi(optarg);
+                break;
+            case 'h':
+                fprintf(stdout, "Usage: %s [-n LINES] [FILE ...]\n", argv[0]);
+                break;
+            case '?':
+                fprintf(stderr, "Usage: %s [-n LINES] [FILE ...]\n", argv[0]);
+                break;
+        }
+    }
+
+    if (optind == argc) {
+        do_head(stdin, nlines);
+    } else {
+        int i;
+
+        for (i = optind; i < argc; i++) {
+            FILE *f;
+
+            f = fopen(argv[i], "r");
+            if (!f) {
+                perror(argv[i]);
+                exit(1);
+            }
+            do_head(f, nlines);
+            fclose(f);
+        }
+    }
+
+    exit(0);
+    /*
     if (argc < 2) {
         fprintf(stderr, "Usage: %s n [file file...]\n", argv[0]);
         exit(1);
@@ -30,6 +76,7 @@ int main(int argc, char *argv[])
         }
     }
     exit(0);
+    */
 }
 
 static void do_head(FILE *f, long nlines)
