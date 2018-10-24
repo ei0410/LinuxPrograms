eb56fcc8ff1b0317d055ba3a43320ff46b055340
ei <chronolavos0124@gmail.com>
2018-10-24 18:12:10 +0900

    what is upcase?

--- httpd.c
+++ httpd.c
@@ -5,6 +5,9 @@
 #include <signal.h>
 #include <errno.h>
 
+#define MAX_REQUEST_BODY_LENGTH 1024
+#define LINE_BUF_SIZE 1024
+
 // structures
 struct HTTPHeaderField {
 	char *name;
@@ -29,6 +32,9 @@ static void trap_signal(int sig, sighandler_t handler);
 static void install_signal_handlers(void);
 static void service(FILE *in, FILE *out, char *docroot);
 static void free_request(struct HTTPRequest *req);
+static struct HTTPHeaderField* read_header_field(FILE *in);
+static void read_request_line(struct HTTPRequest *req, FILE *in);
+static struct HTTPRequest* read_request(FILE *in);
 
 // output log and error
 static void log_exit(char *fmt, ...)
@@ -84,7 +90,7 @@ static void service(FILE *in, FILE *out, char *docroot)
 {
 	struct HTTPRequest *req;
 
-	//req = read_request(in);
+	req = read_request(in);
 	//respond_to(req, out, docroot);
 	free_request(req);
 }
@@ -106,6 +112,90 @@ static void free_request(struct HTTPRequest *req)
 	free(req->path);
 	free(req->body);
 	free(req);
+}
+
+// read header
+static struct HTTPHeaderField* read_header_field(FILE *in)
+{
+	struct HTTPHeaderField *h;
+	char buf[LINE_BUF_SIZE];
+	char *p;
+
+	if (!fgets(buf, LINE_BUF_SIZE, in))
+		log_exit("failed to read request header field: %s", strerror(errno));
+	if ((buf[0] == '\n') || (strcmp(buf, "\r\n") == 0))
+		return NULL;
+
+	p = strchr(buf, ':');
+	if (!p) log_exit("parse error on request header field: %s", buf);
+	*p++ = '\0';
+	h = xmalloc(sizeof(struct HTTPHeaderField));
+	h->name = xmalloc(p - buf);
+	strcpy(h->name, buf);
+
+	p += strspn(p, " \t");
+	h->value = xmalloc(strlen(p) + 1);
+	strcpy(h->value, p);
+
+	return h;
+}
+
+// read request from stream
+static struct HTTPRequest* read_request(FILE *in)
+{
+	struct HTTPRequest *req;
+	struct HTTPHeaderField *h;
+
+	req = xmalloc(sizeof(struct HTTPRequest));
+	read_request_line(req, in);
+	req->header = NULL;
+
+	while (h = read_header_field(in)) {
+		h->next = req->header;
+		req->header = h;
+	}
+	
+	req->length = content_length(req);
+
+	if (req->length != 0) {
+		if (req->length > MAX_REQUEST_BODY_LENGTH)
+			log_exit("request body too long");
+		req->body = xmalloc(req->length);
+		if (fread(req->body, req->length, 1, in) < 1)
+			log_exit("failed to read request body");
+	} else {
+		req->body = NULL;
+	}
+	
+	return req;
+}
+
+// analysis request line
+static void read_request_line(struct HTTPRequest *req, FILE *in)
+{
+	char buf[LINE_BUF_SIZE];
+	char *path, *p;
+
+	if (!fgets(buf, LINE_BUF_SIZE, in))
+		log_exit("no request line");
+	p = strchr(buf, ' ');
+	if (!p) log_exit("parse error on request line (1): %s", buf);
+	*p++ = '\0';
+	req->method = xmalloc(p - buf);
+	strcpy(req->method, buf);
+	upcase(req->method);
+
+	path = p;
+	p = strchr(path, ' ');
+	if (!p) log_exit("parse error on request line (2): %s", buf);
+	*p++ = '\0';
+	req->path = xmalloc(p - path);
+	strcpy(req->path, path);
+
+	if (strncasecmp(p, "HTTP/1.", strlen("HTTP/1.")) != 0)
+		log_exit("parse error on request line (3): %s", buf);
+	p += strlen("HTTP/1.");
+	req->protocol_minor_version = atoi(p);
 }
 
 // main
