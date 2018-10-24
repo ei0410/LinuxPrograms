0baf4673b58640f545ae116464d74828e045b075
ei <chronolavos0124@gmail.com>
2018-10-24 17:15:07 +0900

    add signal

--- httpd.c
+++ httpd.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <stdarg.h>
+#include <signal.h>
+#include <errno.h>
 
 // prototype
 static void log_exit(char *fmt, ...);
 static void* xmalloc(size_t sz);
+static void install_signal_handlers(void);
+static void install_signal_handlers(void);
+static void trap_signal(int sig, sighandler_t handler);
+static void signal_exit(int sig);
 
 // main
 int main(int argc, char *argv[])
@@ -42,4 +49,26 @@ static void* xmalloc(size_t sz)
 	p = malloc(sz);
 	if (!p) log_exit("failed to allocate memory");
 	return p;
+}
+
+// signal process
+static void install_signal_handlers(void)
+{
+	trap_signal(SIGPIPE, signal_exit);
+}
+
+static void trap_signal(int sig, sighandler_t handler)
+{
+	struct sigaction act;
+
+	act.sa_handler = handler;
+	sigemptyset(&act.sa_mask);
+	act.sa_flags = SA_RESTART;
+	if (sigaction(sig, &act, NULL) < 0)
+		log_exit("sigaction() failed: %s", strerror(errno));
+}
+
+static void signal_exit(int sig)
+{
+	log_exit("exit by signal %d", sig);
 }
