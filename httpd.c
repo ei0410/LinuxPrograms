7223af834bae5d9ec5a6183b491191a8b6b2adff
ei <chronolavos0124@gmail.com>
2018-10-25 11:11:02 +0900

    add response but do not define some functions

--- httpd.c
+++ httpd.c
@@ -4,11 +4,20 @@
 #include <stdarg.h>
 #include <signal.h>
 #include <sys/stat.h>
+#include <sys/types.h>
 #include <errno.h>
 #include <ctype.h>
+#include <fcntl.h>
+#include <time.h>
+#include <unistd.h>
 
-#define MAX_REQUEST_BODY_LENGTH 1024
-#define LINE_BUF_SIZE 1024
+#define MAX_REQUEST_BODY_LENGTH (1024 * 1024)
+#define LINE_BUF_SIZE 4096
+#define BLOCK_BUF_SIZE 1024
+#define TIME_BUF_SIZE 64
+#define HTTP_MINOR_VERSION 0 
+#define SERVER_NAME "LittleHTTP"
+#define SERVER_VERSION "1.0"
 
 // structures
 struct HTTPHeaderField {
@@ -48,6 +57,9 @@ static void read_request_line(struct HTTPRequest *req, FILE *in);
 static struct HTTPRequest* read_request(FILE *in);
 static struct FileInfo* get_fileinfo(char *docroot, char *urlpath);
 static char *build_fspath(char *docroot, char *urlpath);
+static void output_common_header_fields(struct HTTPRequest *req, FILE *out, char *status);
+static void do_file_response(struct HTTPRequest *req, FILE *out, char *docroot);
+static void request_to(struct HTTPRequest *req, FILE *out, char *docroot);
 
 // output log and error
 static void log_exit(char *fmt, ...)
@@ -270,6 +282,76 @@ static char *build_fspath(char *docroot, char *urlpath)
 	path = xmalloc(strlen(docroot) + 1 + strlen(urlpath) + 1);
 	sprintf(path, "%s/%s", docroot, urlpath);
 	return path;
+}
+
+// response to http request
+static void request_to(struct HTTPRequest *req, FILE *out, char *docroot)
+{
+	if (strcmp(req->method, "GET") == 0)
+		do_file_response(req, out, docroot);
+	else if (strcmp(req->method, "HEAD") == 0)
+		do_file_response(req, out, docroot);
+	else if (strcmp(req->method, "POST") == 0)
+		method_not_allowed(req, out);
+	else 
+		not_implemented(req, out);
+}
+
+// get doing when file response
+static void do_file_response(struct HTTPRequest *req, FILE *out, char *docroot)
+{
+	struct FileInfo *info;
+
+	info = get_fileinfo(docroot, req->path);
+	if (!info->ok) {
+		free_fileinfo(info);
+		not_found(req, out);
+		return;
+	}
+
+	output_common_header_fields(req, out, "200 OK");
+	fprintf(out, "Content-Length: %ld\r\n", info->size);
+	fprintf(out, "Content-Type: %s\r\n", guess_content_type(info));
+	fprintf(out, "\r\n");
+
+	if (strcmp(req->method, "HEAD") != 0) {
+		int fd;
+		char buf[BLOCK_BUF_SIZE];
+		ssize_t n;
+
+		fd = open(info->path, O_RDONLY);
+		if (fd < 0)
+			log_exit("faild to open %s: %s", info->path, strerror(errno));
+		for (;;) {
+			n = read(fd, buf, BLOCK_BUF_SIZE);
+			if (n < 0)
+				log_exit("failed to read %s: %s", info->path, strerror(errno));
+			if (n == 0)
+				break;
+			if (fwrite(buf, n, 1, out) < n)
+				log_exit("failed to write to socket %s", strerror(errno));
+		}
+		close(fd);
+	}
+	fflush(out);
+	free_fileinfo(info);
+}
+
+// output all common header
+static void output_common_header_fields(struct HTTPRequest *req, FILE *out, char *status)
+{
+	time_t t;
+	struct tm *tm;
+	char buf[TIME_BUF_SIZE];
+
+	t = time(NULL);
+	tm = gmtime(&t);
+	if (!tm) log_exit("gmtime() failed: %s", strerror(errno));
+	strftime(buf, TIME_BUF_SIZE, "%a, %d %b %Y %H:%M:%S GMT", tm);
+	fprintf(out, "HTTP/1.%d %s\r\n", HTTP_MINOR_VERSION, status);
+	fprintf(out, "Date: %s\r\n", buf);
+	fprintf(out, "Server: %s/%s\r\n", SERVER_NAME, SERVER_VERSION);
+	fprintf(out, "Connection: close\r\n");
 }
 
 // main
