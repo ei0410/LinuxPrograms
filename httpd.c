342a11b2c8818436966a37446820e10541624759
ei <chronolavos0124@gmail.com>
2018-10-25 10:43:40 +0900

    add get file infomation

--- httpd.c
+++ httpd.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <stdarg.h>
 #include <signal.h>
+#include <sys/stat.h>
 #include <errno.h>
 #include <ctype.h>
 
@@ -25,6 +26,12 @@ struct HTTPRequest {
 	long length;
 };
 
+struct FileInfo {
+	char *path;
+	long size;
+	int ok;
+};
+
 // prototypes
 static void log_exit(char *fmt, ...);
 static void* xmalloc(size_t sz);
@@ -39,6 +46,8 @@ static struct HTTPHeaderField* read_header_field(FILE *in);
 static void upcase(char *str);
 static void read_request_line(struct HTTPRequest *req, FILE *in);
 static struct HTTPRequest* read_request(FILE *in);
+static struct FileInfo* get_fileinfo(char *docroot, char *urlpath);
+static char *build_fspath(char *docroot, char *urlpath);
 
 // output log and error
 static void log_exit(char *fmt, ...)
@@ -235,6 +244,32 @@ static char* lookup_header_field_value(struct HTTPRequest *req, char *name)
 			return h->value;
 	}
 	return NULL;
+}
+
+// get FileInfo
+static struct FileInfo* get_fileinfo(char *docroot, char *urlpath)
+{
+	struct FileInfo *info;
+	struct stat st;
+
+	info = xmalloc(sizeof(struct FileInfo));
+	info->path = build_fspath(docroot, urlpath);
+	info->ok = 0;
+	if (lstat(info->path, &st) < 0) return info;
+	if(!S_ISREG(st.st_mode)) return info;
+	info->ok = 1;
+	info->size = st.st_size;
+	return info;
+}
+
+// build fspath
+static char *build_fspath(char *docroot, char *urlpath)
+{
+	char *path;
+
+	path = xmalloc(strlen(docroot) + 1 + strlen(urlpath) + 1);
+	sprintf(path, "%s/%s", docroot, urlpath);
+	return path;
 }
 
 // main
